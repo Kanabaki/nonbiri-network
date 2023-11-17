012ae7fc23557922905833ef30e61a2a897bf3b7
Kanabaki <Kagomekanabaki@gmail.com>
2023-11-17 01:23:59 -0600

    completed thought model and started wiring the thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,23 @@
+const { User } = require('../models');
+
+module.exports =  {
+// Here is where you define your routes for Thoughts
+// GET all thoughts
+
+// GET a single thought by id 
+
+// POST or create a new thought
+// (don't forget to push the created thought's _id 
+// to the associated user's thoughts array field)
+
+//  example for POST 
+// {
+//     "thoughtText": "Here's a cool thought...",
+//     "username": "lernantino",
+//     "userId": "5edff358a0fcb779aa7b118b"
+//   }
+
+// PUT to update a thought by id
+
+// DELETE route to remove thought by id
+};
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+// TO DO: const { importThoughtRoutesFromController } = require('../../controllers/thoughtController');
+
+// api/thoughts
+// TO DO: POST route here to create new Thought
+
+
+
+module.exports = router;
