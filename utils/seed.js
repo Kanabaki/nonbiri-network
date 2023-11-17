396df7cf429f7f8eb1ddcce52fbbe01bdb29295e
Kanabaki <Kagomekanabaki@gmail.com>
2023-11-17 02:05:47 -0600

    main branch commit, comments on seed.js

---
+++ utils/seed.js
@@ -0,0 +1,7 @@
+const db = require('../models');
+
+// idk how to seed this...TO DO!
+// need to import models for schema and then the users need to match the
+// schema requirements
+// I probably need an array with objects to seed
+// Would I need reactions or maybe not? 
